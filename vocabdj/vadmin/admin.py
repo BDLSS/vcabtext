aac058ebfc861341d1905e42e9e5ce9bf1f04ec8
Martin Comina <martin.comina@bodleian.ox.ac.uk>
2013-04-11 16:18:15 +0100

    Setup profile models and enable admin interface.

---
+++ vocabdj/vadmin/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from vadmin.models import Title, Location, Profile
+
+admin.site.register(Title)
+admin.site.register(Location)
+admin.site.register(Profile)
--- vocabdj/vadmin/models.py
+++ vocabdj/vadmin/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Title(models.Model):
+    '''A title a person can have, for example: mr, ms, miss, mrs, dr'''
+    title = models.CharField(max_length=20, primary_key=True)
+    notes = models.CharField(max_length=50, blank=True)
+    
+    def __unicode__(self):
+        return self.title
+
+class Location(models.Model):
+    '''A user can work in a particular location or organisation.'''
+    location = models.CharField(max_length=20, primary_key=True)
+    notes = models.CharField(max_length=50, blank=True)
+
+    def __unicode__(self):
+        return self.location
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, unique=True)
+    title = models.ForeignKey(Title)
+    location = models.ForeignKey(Location)
+    notes = models.TextField(blank=True)
+  
+    def __unicode__(self):
+        return '%s %s'%(self.title, self.user)
+    
