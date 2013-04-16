68391f6e6c6b13902bb51afe54ee6380679b9af3
Martin Comina <martin.comina@bodleian.ox.ac.uk>
2013-04-16 10:16:59 +0100

    Define document model and fieldset admin interface.

--- vocabdj/vdata/admin.py
+++ vocabdj/vdata/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from vdata.models import Format, Collection
+from vdata.models import Format, Collection, Document
 
+ENABLE_FIELDSETS = True
 
 class FormatAdmin(admin.ModelAdmin):
     list_display = ('format', 'expanded_acronym', 'html_convert_enable', 'native_mime_type')
@@ -12,4 +13,56 @@ admin.site.register(Format, FormatAdmin)
 class CollectionAdmin(admin.ModelAdmin):
     list_display = ('collection', 'longer_name')
 
-admin.site.register(Collection, CollectionAdmin)    
+admin.site.register(Collection, CollectionAdmin)
+
+
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'namespace')
+    list_filter = ('collection','format')
+    
+    if ENABLE_FIELDSETS:
+        fieldsets = (
+                        (None, {'fields':
+                              ('name', 'namespace', 'format',
+                               'collection', 'brief_description')
+                              }),
+                      
+                        ('Less used general fields', {
+                            'classes': ('collapse', 'wide', 'extrapretty'),
+                            'description': 'If general fields are used less often they can appear here.',
+                            'fields': ('home_url',
+                                       'home_doc_url',
+                                       'description',
+                                       'notes'),
+                              }),
+                      
+                        ('Document contents', {
+                            'classes': ('wide', 'extrapretty'),
+                            'description': 'These are the raw text send to users.',
+                            'fields': ('text',
+                                       'html_enabled',
+                                       'html_doc'),
+                              }),
+                     
+                        ('Auto text options', {
+                            'classes': ('wide', 'extrapretty', 'collapse'),
+                            'description': 'These fields can automatically update the text field. (To be implemented)',
+                            'fields': ('text_fetch_enabled',
+                                       'compress_start_doc',
+                                       'compress_includes_auto_add'),
+                              }),
+                     
+                        ('Version linkage', {
+                            'classes': ('wide', 'extrapretty', 'collapse'),
+                            'description': 'These fields let you make links between documents.',
+                            'fields': ('version_current',
+                                       'version_previous',
+                                       'version_next',
+                                       'version_extends',
+                                       'version_parent',),
+                              }),
+                      )
+
+admin.site.register(Document, DocumentAdmin)
+
+    
--- vocabdj/vdata/models.py
+++ vocabdj/vdata/models.py
@@ -47,3 +47,86 @@ class Collection(models.Model):
     def __unicode__(self):
         return self.collection
     
+class Document(models.Model):
+    '''A representation of vocab document.'''
+    h = 'A short name commonly used to refer to this document. eg. "Dublin Core"'
+    name = models.CharField(max_length=50, help_text=h)
+    
+    h = 'What format is this document.'
+    format = models.ForeignKey(Format, null=True, blank=True, help_text=h)
+    
+    h = 'Documents get grouped into broad collections.'
+    collection = models.ForeignKey(Collection, null=True, blank=True, help_text=h)
+    
+    h = 'If exists, the commonly used namespace for this document. eg "dc" as in xmlns:dc='
+    namespace = models.CharField(max_length=20, blank=True, help_text=h)
+    
+    h = 'A short summary of this document. (50 chars, no HTML tags)'
+    brief_description = models.CharField(max_length=50, blank=True, help_text=h)
+    
+    h = 'An additional summary of this document, if required. '
+    description = models.TextField(blank=True, help_text=h)
+  
+    h = 'You can keep any notes about this document for internal use.'
+    notes = models.TextField(blank=True, help_text=h)
+
+    h = 'If exists, the original url this documents comes from. (Auto fetch uses this).'
+    home_url = models.URLField(blank=True, help_text=h)
+    
+    h = 'If exists, the URL documentation for this original document'
+    home_doc_url = models.URLField(blank=True, help_text=h)
+    
+    
+    # ---------------------------------------------------------------
+    # Fields that hold the native documents, the 1st is V.IMPORTANT
+    # ---------------------------------------------------------------    
+    h = 'The raw document. (Cut and paste from text file editor, or use auto text options.)'
+    text = models.TextField(blank=True, help_text=h)
+
+    h = '*Should the text version be automatically processed and shown as HTML?'
+    html_enabled = models.BooleanField(default=False, help_text=h)
+    
+    h = 'The HTML documentation/representation of this document. (Manual edit is possible).'
+    html_doc = models.TextField(blank=True, help_text=h)
+    
+    # ---------------------------------------------------------------
+    # Fields that automate text loading.
+    # ---------------------------------------------------------------
+    # The following options effect how the field text gets populated.
+    h = '*Reset text from an uploaded a file.'
+    #text_upload = models.FileField(upload_to=None, blank=True, help_text=h)
+    
+    h = '*Reset text by downloading from a home url. (CHECK IT IS SAFE)'
+    text_fetch_enabled = models.BooleanField(default=False, help_text=h)
+    
+    h = '*Reset text from an uploaded compressed file. (accepts zip, gz, bz2)'
+    #compress_upload = models.FileField(upload_to=None, blank=True, help_text=h)
+    
+    h = 'If resetting by compressed file, the main file with the "includes".'
+    compress_start_doc = models.CharField(max_length=30, blank=True, help_text=h)
+    
+    h = '*In addition to the main file, automatically add the included files as documents.'
+    compress_includes_auto_add = models.BooleanField(default=False, help_text=h)
+    
+    # ---------------------------------------------------------------
+    # Fields that link between different version.
+    # ---------------------------------------------------------------
+    h = 'If more than one version of this document exists, what version is this?'
+    version_current = models.CharField(max_length=20, blank=True, help_text=h)
+    
+    h = 'If exists, you can make a link to a PREVIOUS version of this document.'
+    version_previous = models.ForeignKey('self', null=True, blank=True, help_text=h, related_name='previous')
+    
+    h = 'If exists, you can make a link to a NEXT version of this document.'
+    version_next = models.ForeignKey('self', null=True, blank=True, help_text=h, related_name='next')
+    
+    h = 'Does this document extend an existing document?'
+    version_extends = models.ForeignKey('self', null=True, blank=True, help_text=h, related_name='extends')
+    
+    h = 'If this document was automatically added, what document triggered the loading.'
+    version_parent = models.ForeignKey('self', null=True, blank=True, help_text=h, related_name='parent')
+  
+    def __unicode__(self):
+        return '%s - %s'%(self.id, self.name)
+    
+    
