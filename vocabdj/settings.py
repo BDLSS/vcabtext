66471969a167e1dad4cd46238797b62a130031de
Martin Comina <martin.comina@bodleian.ox.ac.uk>
2013-05-07 09:58:28 +0100

    Added static files dirs.

--- vocabdj/settings.py
+++ vocabdj/settings.py
@@ -56,7 +56,7 @@ STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = '/media/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
@@ -68,6 +68,8 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    '/home/hclu0039/Dropbox/Space/vocab/static',
+    '/opt/oxproject/current/static',
 )
 
 # List of finder classes that know how to find static files in
