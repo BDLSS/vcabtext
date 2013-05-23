91b452da392e07b49ca948de4fea33686f34528e
Martin Comina <martin.comina@bodleian.ox.ac.uk>
2013-05-23 16:46:54 +0100

    Where did urls cme from.

--- vocabdj/vocabdj/urls.py
+++ vocabdj/vocabdj/urls.py
@@ -19,6 +19,7 @@ urlpatterns = patterns('',
      url(r'^data/', include('vdata.urls')),
      url(r'^/', include('django.contrib.flatpages.urls')),
      
+     # These urls are are duplicated from inside the application.
      url(r'^(?P<doc_name>.+)/(?P<doc_version>.+)/info/$', views.version_info, name='versioninfo'),
      url(r'^(?P<doc_name>.+)/(?P<doc_version>.+)/$', views.download_version, name='docversion'), 
      url(r'^(?P<document_name>.+)/$', views.download_latest, name='docname'),
