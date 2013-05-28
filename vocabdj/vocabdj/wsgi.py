e848eb20bba09b327d53db4d535341f282439e18
Martin Comina <martin.comina@bodleian.ox.ac.uk>
2013-05-28 12:21:02 +0100

    Added wsgi that will can enable SSO.

---
+++ vocabdj/vocabdj/wsgi.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import django.core.handlers.wsgi
+
+path = '/var/www/vocabdj'
+if path not in sys.path:
+    sys.path.append(path)
+    
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
+
+_application = django.core.handlers.wsgi.WSGIHandler()
+
+
+def application(environ, start_response):
+    
+    if environ.has_key('REMOTE_USER'):
+        os.environ['DF_REMOTE_USER'] = environ['REMOTE_USER']
+    return _application(environ, start_response)
