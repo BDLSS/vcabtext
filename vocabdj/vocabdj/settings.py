50845e164b4cafe777a0dc37e86eb9fc6f647ba9
Martin Comina <martin.comina@bodleian.ox.ac.uk>
2013-05-30 14:44:06 +0100

    Fixed constant 500 error message.

--- vocabdj/vocabdj/settings.py
+++ vocabdj/vocabdj/settings.py
@@ -14,7 +14,7 @@ DATABASES = current.DATABASES
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/{{ docs_version }}/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'vocab-dev.ox.ac.uk', 'vocab.ox.ac.uk']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
