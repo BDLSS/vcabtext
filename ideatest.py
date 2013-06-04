2272397241689d722241d6e03ebf4b803c112b90
Martin Comina <martin.comina@bodleian.ox.ac.uk>
2013-06-04 16:08:35 +0100

    Test xslt transform.

--- ideatest.py
+++ ideatest.py
@@ -27,5 +27,72 @@ def get_styles():
         
         print('Style successfully generated.')
         
+def get_lexers():
+    ''''''
+    import pygments.lexers as t
+    count = 0 
+    for item in t.get_all_lexers():
+        print item
+        count += 1
+    try:
+        print t.get_lexer_for_filename('this.xsd')
+    except:
+        print 'found'
+    print 'NUMBER OF LEXERS: %s'%count
+
+def rdf_maker():
+    '''Test how to convert rdf using xslt'''
+    import StringIO
+    #Example from http://www.w3schools.com/rdf/rdf_example.asp
+    rdf = StringIO.StringIO("""<?xml version="1.0"?>
+
+<rdf:RDF
+xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+xmlns:cd="http://www.recshop.fake/cd#">
+
+<rdf:Description
+rdf:about="http://www.recshop.fake/cd/Empire Burlesque">
+  <cd:artist>Bob Dylan</cd:artist>
+  <cd:country>USA</cd:country>
+  <cd:company>Columbia</cd:company>
+  <cd:price>10.90</cd:price>
+  <cd:year>1985</cd:year>
+</rdf:Description>
+
+<rdf:Description
+rdf:about="http://www.recshop.fake/cd/Hide your heart">
+  <cd:artist>Bonnie Tyler</cd:artist>
+  <cd:country>UK</cd:country>
+  <cd:company>CBS Records</cd:company>
+  <cd:price>9.90</cd:price>
+  <cd:year>1988</cd:year>
+</rdf:Description>
+</rdf:RDF>
+""")
+    
+    from lxml import etree
+    doc = etree.parse(rdf)
         
-get_styles()
+    xslt_root = etree.XML('''<xsl:stylesheet version="1.0"
+     xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+     <xsl:template match="/">
+If you are reading this <eggs><xsl:value-of select="/a/b/text()" /></eggs>
+it WORKS1.
+     </xsl:template>
+it WORKS2.
+ </xsl:stylesheet>''')
+    
+    try:
+        transform = etree.XSLT(xslt_root)
+        content = transform(doc)
+        print content
+    except etree.Error, e:
+        print 'ERROR:  %s'%e
+    
+    
+    
+
+#get_styles()
+get_lexers()
+print '-'*10
+rdf_maker()
