da81ecdac5742fab9271b783c0afca011da6b21f
Martin Comina <martin.comina@bodleian.ox.ac.uk>
2013-06-18 20:08:15 +0100

    Move URL to download latest item.

--- vocabdj/vdata/urls.py
+++ vocabdj/vdata/urls.py
@@ -11,7 +11,7 @@ urlpatterns = patterns('',
     url(r'^(?P<document_id>\d+)/download/$', views.download, name='down'),
     url(r'^collects/$', views.collections, name='collects'),
     url(r'^collects/(?P<collection_collection>.+)/$', views.collection, name='collect'),
+    url(r'^(?P<document_name>.+)/current/$', views.download_latest, name='docname'),
     url(r'^(?P<doc_name>.+)/(?P<doc_version>.+)/info/$', views.version_info, name='versioninfo'),
     url(r'^(?P<doc_name>.+)/(?P<doc_version>.+)/$', views.download_version, name='docversion'), 
-    url(r'^(?P<document_name>.+)/$', views.download_latest, name='docname'),    
 )
--- vocabdj/vocabdj/urls.py
+++ vocabdj/vocabdj/urls.py
@@ -20,7 +20,7 @@ urlpatterns = patterns('',
      url(r'^/', include('django.contrib.flatpages.urls')),
      
      # These urls are are duplicated from inside the application.
+     url(r'^(?P<document_name>.+)/current/$', views.download_latest, name='docname'),
      url(r'^(?P<doc_name>.+)/(?P<doc_version>.+)/info/$', views.version_info, name='versioninfo'),
      url(r'^(?P<doc_name>.+)/(?P<doc_version>.+)/$', views.download_version, name='docversion'), 
-     url(r'^(?P<document_name>.+)/$', views.download_latest, name='docname'),
 )
